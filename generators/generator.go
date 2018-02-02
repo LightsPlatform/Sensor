a41cd5665ec7eb35fed28d0f8064ea8ffcf5a8d0
Parham Alvani <parham.alvani@gmail.com>
2018-02-02 13:24:17 +0330

    Adds generation interface.

---
+++ generators/generator.go
@@ -0,0 +1,18 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 02-02-2018
+ * |
+ * | File Name:     generators/generator.go
+ * +===============================================
+ */
+
+package generator
+
+// Generator provides a general way for generating load
+// Generate calls in each iteration and returns number
+// of packets that must be generated.
+type Generator interface {
+	Generate() int
+}
