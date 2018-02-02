9882689018e99e080452349788bee52441dc4031
Parham Alvani <parham.alvani@gmail.com>
2018-02-02 13:08:17 +0330

    Adds basis of sensor structure.

---
+++ sensor/sensor.go
@@ -0,0 +1,18 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 02-02-2018
+ * |
+ * | File Name:     sensor/sensor.go
+ * +===============================================
+ */
+
+package sensor
+
+// Sensor represents virtual sensor that
+// only generate random data with given generator
+type Sensor struct {
+	Name string
+	// TODO Generator
+}
