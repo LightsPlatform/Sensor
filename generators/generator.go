501e04371b6e215a947fad4cfe3612760fc16438
Parham Alvani <parham.alvani@gmail.com>
2018-02-18 07:03:23 +0330

    Adds uniform generator.

--- generators/generator.go
+++ generators/generator.go
@@ -10,9 +10,34 @@
 
 package generators
 
+import "time"
+
 // Generator provides a general way for generating load
-// Generate calls in each iteration and returns number
-// of packets that must be generated.
+// Generate runs new go routine that passes number
+// of packets that must be generated into channel.
 type Generator interface {
 	Generate() <-chan int
+}
+
+// UniformGenerator generates traffic uniformaly based on time
+type UniformGenerator struct {
+	Timeslot time.Duration
+}
+
+// Generate runs new go routine that passes number
+// of packets that must be generated into channel.
+func (u UniformGenerator) Generate() <-chan int {
+	g := make(chan int, 0)
+	t := time.Tick(u.Timeslot)
+
+	go func() {
+		for {
+			select {
+			case <-t:
+				g <- 1
+			}
+		}
+	}()
+
+	return g
 }
